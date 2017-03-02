d86c145c5abc438053aa3fb30637d2cd08528e6a
AndrewTanchenko <oto.kun.sama@gmail.com>
2017-03-02 21:56:17 +0200

    Update Vector.java

--- Vector.java
+++ Vector.java
@@ -1,61 +1,68 @@
 // Javacore / Tanchenko A.
+// updated 02Mar17
 
 import java.util.Arrays;
 
 class Vector {
   
-    private int x;
-    private int y;
-    private int[] v = {x,y};
+    private double x;
+    private double y;
+    private double[] v = {x,y};
   
   public Vector(){ 
-    //this.x=0;
-    //this.y=0;
+    this.x=0;
+    this.y=0;
     this.v[0]=0;
     this.v[1]=0;
   }
   
-  public Vector(int x, int y){ 
-    //this.x=x;
-    //this.y=y;
+  public Vector(double x, double y){ 
+    this.x=x;
+    this.y=y;
     this.v[0]=x;
     this.v[1]=y;
   }
   
+  /*
+  public static void CreateVectorArray(String arrV, int num){ 
+    Vector[] arrV = new Vector[num];
+    for(int i=0; i<=num-1;i++){
+      arrV[i] = new Vector();
+    }
+  }
+  */
+    
   public static void printVector(Vector a){
-    int[] v_ = new int[2];
+    double[] v_ = new double[2];
     v_[0]=a.getX();
     v_[1]=a.getY();
     System.out.println(Arrays.toString(v_));
   }
   
   public String toString(){
-    return Arrays.toString(getXY());
+    return Arrays.toString(this.v);
   }
   
-  public void setXY(int x, int y){
+  public void setXY(double x, double y){
     this.v[0]=x;
     this.v[1]=y;
   }
-  public void setX(int x){
+  public void setX(double x){
     this.v[0]=x;
   }
-  public void setY(int y){
+  public void setY(double y){
     this.v[1]=y;
   }
   
-  public int[] getXY(){
-  return v;
+  public double getX(){
+  return this.v[0];
   }
-  public int getX(){
-  return v[0];
-  }
-  public int getY(){
-  return v[1];
+  public double getY(){
+  return this.v[1];
   }
   
-  public int length(){
-  return (int)(Math.sqrt(v[0]*v[0]+v[1]*v[1]));
+  public double length(){
+  return (double)(Math.sqrt(v[0]*v[0]+v[1]*v[1]));
   }
   
   public Vector add(Vector a){
@@ -66,17 +73,15 @@ class Vector {
   return new Vector(v[0]-a.getX(),v[1]-a.getY());
   }
         
-  public int[] constMult(int c){
-    v[0]=v[0]*c;
-    v[1]=v[1]*c;
-  return v;
+  public Vector constMult(double c){
+  return new Vector(getX()*c,getY()*c);
   }
   
-  public static int scalarMult(Vector a,Vector b){
-  return (int)(a.getX()*b.getX()+a.getY()*b.getY());
+  public static double scalarMult(Vector a,Vector b){
+  return (double)(a.getX()*b.getX()+a.getY()*b.getY());
   }
   
-  public static boolean checkCollinear(Vector a,Vector b){
+  public static boolean isCollinear(Vector a,Vector b){
     boolean ans_=false;
     if((float)(a.getX())/b.getX()==(float)(a.getY())/b.getY()){
       ans_=true;
@@ -84,7 +89,7 @@ class Vector {
   return ans_;
   }
   
-  public static boolean checkOrtogonal(Vector a,Vector b){
+  public static boolean isOrtogonal(Vector a,Vector b){
     boolean ans_=false;
     if(scalarMult(a,b)==0){
       ans_=true;
@@ -95,55 +100,54 @@ class Vector {
 //////////////////////////////////////////////////////
   
   public static void main(String[] args) {
+    
+    Vector a0 = new Vector();
     Vector a = new Vector(1,0);
     Vector b = new Vector(0,4);
     
+    //Vector[] arrV = new Vector[2];
+    //CreateVectorArray(arrV,2);
     Vector[] arrV = {a,b};
     
     
     printVector(arrV[0]);
 
-    System.out.println ("Vector a= "+a.toString());
+    System.out.println ("Vector a= "+a);
     //printVector(a);
-    System.out.println ("Vector b= "+b.toString());
+    System.out.println ("Vector b= "+b);
     //printVector(b);
     
-    if(checkCollinear(a,b)){
+    if(isCollinear(a,b)){
       System.out.println ("Vectors a and b are collinear");
     }
     else{
       System.out.println ("Vectors a and b aren't collinear");
     }
     
-    if(checkOrtogonal(a,b)){
+    if(isOrtogonal(a,b)){
       System.out.println ("Vectors a and b are ortogonal");
     }
     else{
       System.out.println ("Vectors a and b aren't ortogonal");
     }
     
-    System.out.println ("Vector a+b= ");
-    printVector(a.add(b));
+    System.out.println ("Vector a+b= "+a.add(b));
     
-    System.out.println ("Vector a-b= ");
-    printVector(a.sub(b));
+    System.out.println ("Vector a-b= "+a.sub(b));
     
     System.out.println ("Scalar mult a*b= "+scalarMult(a,b));
     
-    System.out.println ("Vector set a=[2,3] ");
-    a.setXY(2,3);
-    printVector(a);
+    a.setXY(3,4);
+    System.out.println ("Vector set a=[2,3] "+a);
+    //printVector(a);
     
     System.out.println ("length a= "+a.length());
     
-    int n=5;
-    System.out.println ("Vector a*"+n+"= ");
-    a.constMult(n);
+    double n=5;
+    System.out.println ("Vector a*"+n+"= "+a.constMult(n));
+    //a.constMult(n);
     printVector(a);
     
-    
-    //System.out.println ("a(x,y)= "+a.getX()+","+a.getY());
-    //System.out.println ("b.y= "+b.getY());
     
   }
 }
